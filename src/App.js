f2291084989b2565a77529682f7ff03913aee2bc
hareesh-r <hareeshprogrammer@gmail.com>
2021-08-10 17:30:36 +0530

    header and search0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React, { Component } from 'react'
+import sliderimg from "./img/sliderimg.jpeg"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+export class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="header__left">
+            <img src="https://internshala.com/static/images/common/new_internshala_logo.svg" alt="" />
+          </div>
+          <div className="header__right">
+            <div className="header__right__link">Internships</div>
+            <div className="header__right__link">Online Trainings <span className="orange">OFFER</span></div>
+            <div className="header__right__link">Fresher Jobs</div>
+            <button className="header__login__button1">Login</button>
+            <button className="header__login__button2">Register</button>
+          </div>
+        </header>
+        <div className="search__box flex__center">
+          <input type="text" placeholder="What are you looking for ?" />
+          <button>
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+
+        <div className="carousel">
+          
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
